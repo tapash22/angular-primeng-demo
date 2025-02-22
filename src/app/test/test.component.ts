1052e92f8311d71ed9048427c7c8f044f1cd058a
tapash22 <tapasp263@gmail.com>
2025-02-22 22:36:53 +0600

    update

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,77 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as THREE from 'three';
-
 
 @Component({
   selector: 'app-test',
-  imports: [],
   templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements AfterViewInit {
+  @ViewChild('canvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
 
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private spheres : THREE.Mesh[]=[];
+
+  ngAfterViewInit(): void {
+    this.initThree();
+  }
+
+  private initThree() {
+    const canvas = this.canvasRef.nativeElement;
+
+    this.renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+    this.renderer.setSize(500, 500);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    this.camera.position.set(0, 0, 5);
+    this.camera.lookAt(0, 0, 0);
+
+    const textureLoader = new THREE.TextureLoader();
+    
+    // Load the texture and apply it to the sphere
+    textureLoader.load('images/saturnring.png', (texture) => {
+      this.createSphere(texture);
+      this.animate();
+    });
+  }
+
+  private createSphere(texture: THREE.Texture) {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(1, 1);
+    const geometry = new THREE.SphereGeometry(2, 24, 24); // Create a sphere
+    const material = new THREE.MeshBasicMaterial({
+      map: texture,
+    });
+
+   // Create 4 spheres and position them
+   const positions = [
+    [-3, 3, 0],  // Top-left
+    [3, 3, 0],   // Top-right
+    [-3, -3, 0], // Bottom-left
+    [3, -3, 0]   // Bottom-right
+  ];
+
+  positions.forEach((pos) => {
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.position.set(pos[0], pos[1], pos[2]);
+    this.scene.add(sphere);
+    this.spheres.push(sphere);
+  });
+  }
+
+  private animate = () => {
+    requestAnimationFrame(this.animate);
+
+    this.spheres.forEach((sphere) => {
+      sphere.rotation.y += 0.02;
+    });
+
+    this.renderer.render(this.scene, this.camera);
+  };
 }
